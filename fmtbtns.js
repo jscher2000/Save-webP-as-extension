ace733b79a3ecfacecaabd002bed40da694b4eef
Jefferson Scher <jscher2000@gmail.com>
2020-05-16 12:58:22 -0700

    verison 0.6
    Options for what a plain click, Shift+click, and Ctrl+click on the context menu item do. Highlighting for unsaved changes.

--- fmtbtns.js
+++ fmtbtns.js
@@ -1,12 +1,17 @@
 /* 
   Copyright 2020. Jefferson "jscher2000" Scher. License: MPL-2.0.
   version 0.5 - fifth try
+  version 0.6 - options for menu item behavior, highlight unsaved options page changes
 */
 
 /**** Create and populate data structure ****/
 
 // Default starting values
 var oPrefs = {
+	/* menu action */
+	menuplain: 'showbar',		// show button bar
+	menushift: 'savepng',		// save png
+	menuctrl: 'savejpg92',		// save jpeg at 92%
 	/* Format buttons for overlay bar, color scheme */
 	btnpng: true,				// show PNG button
 	btnjpg100: true,			// show JPG 100% button
@@ -53,25 +58,144 @@ browser.menus.create({
 });
 // Inject styles and content script for right-clicked image
 browser.menus.onClicked.addListener((menuInfo, currTab) => {
-	var cssfile = '/light.css';
-	if (oPrefs.btndark == true) cssfile = '/dark.css';
-	var btns = [];
-	if (oPrefs.btnpng == true) btns.push({params: 'p,1', label: 'PNG', span: null});
-	if (oPrefs.btnjpg100 == true) btns.push({params: 'j,1', label: 'JPG', span: '100'});
-	if (oPrefs.btnjpg92 == true) btns.push({params: 'j,0.92', label: 'JPG', span: '92%'});
-	if (oPrefs.btnjpg85 == true) btns.push({params: 'j,0.85', label: 'JPG', span: '85%'});
-	if (oPrefs.btnjpg80 == true) btns.push({params: 'j,0.80', label: 'JPG', span: '80%'});
-	if (oPrefs.btnjpg75 == true) btns.push({params: 'j,0.75', label: 'JPG', span: '75%'});
-	btns.push({params: 'options', label: '⚙️', span: null});
-	btns.push({params: 'close', label: 'X', span: null});
+	// Check modifiers to determine action
+	var axn = 'showbar';
+	switch (menuInfo.modifiers.length){
+		case 0: //Plain
+			axn = oPrefs.menuplain;
+			break;
+		case 1: // Shift or Ctrl
+			if (menuInfo.modifiers.includes('Shift')){
+				axn = oPrefs.menushift;
+			} else if ((browser.runtime.PlatformOs == 'mac' && menuInfo.modifiers.includes('Command')) ||
+						(browser.runtime.PlatformOs != 'mac' && menuInfo.modifiers.includes('Ctrl'))){
+				axn = oPrefs.menuctrl;
+			} else {
+				// What is the user trying? Just show the bar.
+			}
+			break;
+		default:
+			// User held down two modifier keys? Just show the bar.
+	}
+	// Implement 'showbar'
+	if (axn == 'showbar'){
+		var cssfile = '/light.css';
+		if (oPrefs.btndark == true) cssfile = '/dark.css';
+		var btns = [];
+		if (oPrefs.btnpng == true) btns.push({params: 'p,1', label: 'PNG', span: null});
+		if (oPrefs.btnjpg100 == true) btns.push({params: 'j,1', label: 'JPG', span: '100'});
+		if (oPrefs.btnjpg92 == true) btns.push({params: 'j,0.92', label: 'JPG', span: '92%'});
+		if (oPrefs.btnjpg85 == true) btns.push({params: 'j,0.85', label: 'JPG', span: '85%'});
+		if (oPrefs.btnjpg80 == true) btns.push({params: 'j,0.80', label: 'JPG', span: '80%'});
+		if (oPrefs.btnjpg75 == true) btns.push({params: 'j,0.75', label: 'JPG', span: '75%'});
+		btns.push({params: 'options', label: '⚙️', span: null});
+		btns.push({params: 'close', label: 'X', span: null});
 
-	browser.tabs.insertCSS({
-			file: cssfile,
-			frameId: menuInfo.frameId,
-			cssOrigin: "user"
-	}).then(() => {
+		browser.tabs.insertCSS({
+				file: cssfile,
+				frameId: menuInfo.frameId,
+				cssOrigin: "user"
+		}).then(() => {
+			browser.tabs.executeScript({
+				code:  `/* Save webP as... v0.6 */
+						// Set up variables from menu click
+						var w = browser.menus.getTargetElement(${menuInfo.targetElementId});
+						var u = new URL('${menuInfo.srcUrl}');
+						function convert_${menuInfo.targetElementId}(el, path, fmt, ext, qual){
+							// Create new filename
+							var f = path.slice(path.lastIndexOf('/') + 1).replace(/\.webp/i, '_webp').replace(/\.png/i, '_png').replace(/\.jpg/i, '_jpg').replace(/\.gif/i, '_gif');
+							if (qual != 1) f += '_' + (100 * parseFloat(qual));
+							f += '.' + ext;
+							// Create canvas
+							var canv = document.createElement('canvas');
+							canv.width = w.naturalWidth;
+							canv.height = w.naturalHeight;
+							var ctx = canv.getContext('2d');
+							if (ext == 'jpg'){
+								// Match the background color (fix "white" to avoid transparency)
+								var b = window.getComputedStyle(w).getPropertyValue('background-color');
+								if (b === 'rgba(0, 0, 0, 0)') b = '#fff';
+								ctx.fillStyle = b;
+								ctx.fillRect(0, 0, canv.width, canv.height);
+							}
+							// Then add the image
+							ctx.drawImage(w, 0, 0);
+							canv.toBlob((blob) => {
+								// Send blob to background script for downloading
+								browser.runtime.sendMessage({
+									"download": {
+										cblob: blob,
+										fname: f
+									}
+								})
+								.catch((err) => {console.log('An error occurred while saving the image: '+err.message);});
+							}, fmt, qual);
+						}
+						function convbtn_${menuInfo.targetElementId}(e){
+							// Execute button click (save image or close)
+							tgt = e.target;
+							if (!tgt.hasAttribute('params')) tgt = tgt.closest('button');
+							var params = tgt.getAttribute('params').split(',');
+							if (params[0] == 'p') convert_${menuInfo.targetElementId}(w, u.pathname, 'image/png', 'png', 1);
+							else if (params[0] == 'j'){
+								convert_${menuInfo.targetElementId}(w, u.pathname, 'image/jpeg', 'jpg', parseFloat(params[1]));
+							} else if (params[0] == 'options'){
+								browser.runtime.sendMessage({"options": "show"});
+							} else if (params[0] == 'close'){
+								var bar = document.getElementById('btns_${menuInfo.targetElementId}');
+								if (bar){
+									bar.removeEventListener('click', convbtn_${menuInfo.targetElementId}, false);
+									bar.remove();
+								}
+								window.removeEventListener('resize', setpos_${menuInfo.targetElementId}, false);
+							}
+						}
+						function setpos_${menuInfo.targetElementId}(e){
+							// Line up the button bar with the top of the image
+							var tgt = document.getElementById('btns_${menuInfo.targetElementId}');
+							if (!tgt) return;
+							var br = w.getBoundingClientRect();
+							tgt.style.top = window.scrollY + br.top + 'px';
+							tgt.style.left = window.scrollX + br.left + 'px';
+							tgt.style.width = br.width + 'px';
+							// Make sure the bar is in view
+							if (br.top < 0 || br.top > window.innerHeight) tgt.scrollIntoView();
+						}
+						/* This only works on some sites, so unfortunately, we need to use <all_urls>
+						// Avoid canvas taint from cross-site images
+						if (u.hostname != location.hostname) w.setAttribute('crossorigin', 'Anonymous');
+						*/
+						// Create button bar and position it
+						var d = document.createElement('div');
+						d.id = 'btns_${menuInfo.targetElementId}'; 
+						d.className = 'saveWebPasbtns';
+						var btns = ${JSON.stringify(btns)};
+						for (var i=0; i<btns.length; i++){
+							var b = document.createElement('button');
+							b.setAttribute('params', btns[i].params);
+							b.appendChild(document.createTextNode(btns[i].label));
+							if (btns[i].span){
+								var s = document.createElement('span');
+								s.appendChild(document.createTextNode(btns[i].span));
+								b.appendChild(s);
+							}
+							d.appendChild(b);
+						}
+						document.body.appendChild(d);
+						setpos_${menuInfo.targetElementId}();
+						// Set up event listeners
+						d.addEventListener('click', convbtn_${menuInfo.targetElementId}, false);
+						window.addEventListener('resize', setpos_${menuInfo.targetElementId}, false);
+						'WTF'`
+			});
+		}).catch((err) => {
+			browser.tabs.executeScript({
+				code: `alert("Apologies, but it didn't work. Firefox says: '${err}'");`
+			})
+		});
+	} else { // Use the specified format and quality
 		browser.tabs.executeScript({
-			code:  `/* Save webP as... v0.5 */
+			code:  `/* Save webP as... v0.6 */
 					// Set up variables from menu click
 					var w = browser.menus.getTargetElement(${menuInfo.targetElementId});
 					var u = new URL('${menuInfo.srcUrl}');
@@ -105,68 +229,24 @@ browser.menus.onClicked.addListener((menuInfo, currTab) => {
 							.catch((err) => {console.log('An error occurred while saving the image: '+err.message);});
 						}, fmt, qual);
 					}
-					function convbtn_${menuInfo.targetElementId}(e){
-						// Execute button click (save image or close)
-						tgt = e.target;
-						if (!tgt.hasAttribute('params')) tgt = tgt.closest('button');
-						var params = tgt.getAttribute('params').split(',');
-						if (params[0] == 'p') convert_${menuInfo.targetElementId}(w, u.pathname, 'image/png', 'png', 1);
-						else if (params[0] == 'j'){
-							convert_${menuInfo.targetElementId}(w, u.pathname, 'image/jpeg', 'jpg', parseFloat(params[1]));
-						} else if (params[0] == 'options'){
-							browser.runtime.sendMessage({"options": "show"});
-						} else if (params[0] == 'close'){
-							var bar = document.getElementById('btns_${menuInfo.targetElementId}');
-							if (bar){
-								bar.removeEventListener('click', convbtn_${menuInfo.targetElementId}, false);
-								bar.remove();
-							}
-							window.removeEventListener('resize', setpos_${menuInfo.targetElementId}, false);
+					var fmt = '${axn}'.slice(4); //Past the word save
+					if (fmt == 'png'){
+						convert_${menuInfo.targetElementId}(w, u.pathname, 'image/png', 'png', 1);
+					} else {
+						if (fmt.slice(0,3) == 'jpg'){
+							var qual = parseFloat(fmt.slice(3)) / 100;
+							convert_${menuInfo.targetElementId}(w, u.pathname, 'image/jpeg', 'jpg', qual);
+						} else {
+							alert('Sorry, but I did not recognize the desired format from ' + fmt);
 						}
 					}
-					function setpos_${menuInfo.targetElementId}(e){
-						// Line up the button bar with the top of the image
-						var tgt = document.getElementById('btns_${menuInfo.targetElementId}');
-						if (!tgt) return;
-						var br = w.getBoundingClientRect();
-						tgt.style.top = window.scrollY + br.top + 'px';
-						tgt.style.left = window.scrollX + br.left + 'px';
-						tgt.style.width = br.width + 'px';
-						// Make sure the bar is in view
-						if (br.top < 0 || br.top > window.innerHeight) tgt.scrollIntoView();
-					}
-					/* This only works on some sites, so unfortunately, we need to use <all_urls>
-					// Avoid canvas taint from cross-site images
-					if (u.hostname != location.hostname) w.setAttribute('crossorigin', 'Anonymous');
-					*/
-					// Create button bar and position it
-					var d = document.createElement('div');
-					d.id = 'btns_${menuInfo.targetElementId}'; 
-					d.className = 'saveWebPasbtns';
-					var btns = ${JSON.stringify(btns)};
-					for (var i=0; i<btns.length; i++){
-						var b = document.createElement('button');
-						b.setAttribute('params', btns[i].params);
-						b.appendChild(document.createTextNode(btns[i].label));
-						if (btns[i].span){
-							var s = document.createElement('span');
-							s.appendChild(document.createTextNode(btns[i].span));
-							b.appendChild(s);
-						}
-						d.appendChild(b);
-					}
-					document.body.appendChild(d);
-					setpos_${menuInfo.targetElementId}();
-					// Set up event listeners
-					d.addEventListener('click', convbtn_${menuInfo.targetElementId}, false);
-					window.addEventListener('resize', setpos_${menuInfo.targetElementId}, false);
 					'WTF'`
+		}).catch((err) => {
+			browser.tabs.executeScript({
+				code: `alert("Apologies, but the quick save didn't work. Firefox says: '${err}'");`
+			})
 		});
-	}).catch((err) => {
-		browser.tabs.executeScript({
-			code: `alert("Apologies, but it didn't work. Firefox says: '${err}'");`
-		})
-	});
+	}
 });
 
 /**** Handle Requests from Content and Options ****/
@@ -197,33 +277,30 @@ function handleMessage(request, sender, sendResponse){
 			if (oPrefs.keepprivate === false) opts.incognito = false;
 			else opts.incognito = true;
 		}
-		//console.log(opts);
 		browser.downloads.download(opts).catch((err) => {
 			console.log('An error occurred while saving the image: '+err.message);
 		});
 	} else if ("options" in request) {
 		browser.runtime.openOptionsPage();
-	} else if ("get" in request) {
-		// Send oPrefs to Options page
-		sendResponse({
-			prefs: oPrefs
+		/* 
+		browser.tabs.create({
+			url: browser.runtime.getURL('options.html')
 		});
+		*/
 	} else if ("update" in request) {
-		// Receive pref updates from Options page, store to oPrefs, and commit to storage
-		var oSettings = request["update"];
-		oPrefs.btnpng = oSettings.btnpng;
-		oPrefs.btnjpg100 = oSettings.btnjpg100;
-		oPrefs.btnjpg92 = oSettings.btnjpg92;
-		oPrefs.btnjpg85 = oSettings.btnjpg85;
-		oPrefs.btnjpg80 = oSettings.btnjpg80;
-		oPrefs.btnjpg75 = oSettings.btnjpg75;
-		oPrefs.btndark = oSettings.btndark;
-		oPrefs.saveas = oSettings.saveas;
-		oPrefs.usefolder = oSettings.usefolder;
-		// More to be added here in the future
-		oPrefs.keepprivate = oSettings.keepprivate;
-		browser.storage.local.set({prefs: oPrefs})
-			.catch((err) => {console.log('Error on browser.storage.local.set(): '+err.message);});
+		// Receive pref hint from Options page and update oPrefs from storage
+		if (request["update"] == 'fromStorage'){
+			browser.storage.local.get("prefs").then((results) => {
+				if (results.prefs != undefined){
+					if (JSON.stringify(results.prefs) != '{}'){
+						var arrSavedPrefs = Object.keys(results.prefs)
+						for (var j=0; j<arrSavedPrefs.length; j++){
+							oPrefs[arrSavedPrefs[j]] = results.prefs[arrSavedPrefs[j]];
+						}
+					}
+				}
+			}).catch((err) => {console.log('Error retrieving "prefs" from storage: '+err.message);});
+		}
 	}
 }
 browser.runtime.onMessage.addListener(handleMessage);
--- options.js
+++ options.js
@@ -1,13 +1,54 @@
-/* Save webP as PNG or JPEG v0.5 */
+/* Save webP as PNG or JPEG v0.6 */
 
-// Set form values from current preferences
-browser.runtime.sendMessage({
-	get: "oPrefs"
-}).then((response) => {
-	var oSettings = response['prefs'];
+/*** Initialize Page ***/
+
+// Default starting values
+var oSettings = {
+	/* menu action */
+	menuplain: 'showbar',		// show button bar
+	menushift: 'savepng',		// save png
+	menuctrl: 'savejpg92',		// save jpeg at 92%
+	/* Format buttons for overlay bar, color scheme */
+	btnpng: true,				// show PNG button
+	btnjpg100: true,			// show JPG 100% button
+	btnjpg92: true,				// show JPG 92% button
+	btnjpg85: true,				// show JPG 85% button
+	btnjpg80: true,				// show JPG 80% button
+	btnjpg75: true,				// show JPG 75% button
+	btndark: false,				// show dark buttons
+	/* Save dialog, path, file name options */
+	saveas: null,				// SaveAs parameter for Download() yes/no/null
+	usefolder: true,			// subfolder for download
+	customfolder: null,			// custom subfolder name
+	subfolder: null,			// Date/Host/ImgServer/None
+	namedate: false,			// Add date into file name
+	nametime: false,			// Add time into file name
+	namehost: false,			// Add host into file name
+	nameimg: false,				// Add image server into file name
+	/* Other options */
+	keepprivate: true			// Don't add downloads from incognito to history
+}
+
+// Update oSettings from storage
+browser.storage.local.get("prefs").then( (results) => {
+	if (results.prefs != undefined){
+		if (JSON.stringify(results.prefs) != '{}'){
+			var arrSavedPrefs = Object.keys(results.prefs)
+			for (var j=0; j<arrSavedPrefs.length; j++){
+				oSettings[arrSavedPrefs[j]] = results.prefs[arrSavedPrefs[j]];
+			}
+		}
+	}
+}).then(() => {
+	// Context menu select's
+	var sels = document.querySelectorAll('select');
+	for (var i=0; i<sels.length; i++){
+		var selopt = document.querySelector('select[name="' + sels[i].name + '"] option[value="' + oSettings[sels[i].name] + '"]');
+		selopt.setAttribute('selected', 'selected');
+	}
 	// Checkboxes
 	var chks = document.querySelectorAll('.chk input[type="checkbox"]');
-	for (var i=0; i<chks.length; i++){
+	for (i=0; i<chks.length; i++){
 		if (oSettings[chks[i].name] == true) chks[i].checked = true;
 		else chks[i].checked = false;
 	}
@@ -35,17 +76,23 @@ browser.runtime.sendMessage({
 	// More to come later
 	
 	// Privacy
-	if (oSettings.keepprivate == true) document.forms[0].radPrivate.value = 'yes';
-	else document.forms[0].radPrivate.value = 'no';
+	document.forms[0].radPrivate.value = oSettings.keepprivate;
 }).catch((err) => {
-	console.log('Problem getting settings: '+err.message);
+	console.log('Error retrieving "prefs" from storage: '+err.message);
 });
 
-// Send changes to background for storage
+/*** Handle User Actions ***/
+
+// Update storage
 function updatePref(evt){
+	if (evt.target.className != 'savebtn') return;
+	// Context menu select's
+	var sels = document.querySelectorAll('select');
+	for (var i=0; i<sels.length; i++){
+		oSettings[sels[i].name] = sels[i].value;
+	}
 	// Checkboxes
 	var chks = document.querySelectorAll('.chk input[type="checkbox"]');
-	var oSettings = {};
 	for (var i=0; i<chks.length; i++){
 		oSettings[chks[i].name] = chks[i].checked;
 	}
@@ -72,13 +119,111 @@ function updatePref(evt){
 	if (document.forms[0].radPrivate.value == 'yes') oSettings.keepprivate = true;
 	else oSettings.keepprivate = false;
 	
-	// Send update to background
-	browser.runtime.sendMessage({
-		update: oSettings
+	// Update storage
+	browser.storage.local.set(
+		{prefs: oSettings}
+	).then(() => {
+		// Clean up highlighting
+		var lbls = document.querySelectorAll('label');
+		for (var i=0; i<lbls.length; i++){
+			lbls[i].style.backgroundColor = '';
+		}
+		var btns = document.getElementsByClassName('savebtn');
+		for (i=0; i<btns.length; i++){
+			btns[i].style.backgroundColor = '';
+		}
+		evt.target.blur();
+		// Send update to background
+		browser.runtime.sendMessage({
+			update: 'fromStorage'
+		});
+	}).catch((err) => {
+		console.log('Error on browser.storage.local.set(): ' + err.message);
 	});
 }
-// Attach event handler to the Save button
-document.getElementById('btnSave1').addEventListener('click', updatePref, false);
-document.getElementById('btnSave2').addEventListener('click', updatePref, false);
-document.getElementById('btnSave3').addEventListener('click', updatePref, false);
-document.getElementById('btnSave4').addEventListener('click', updatePref, false);
+
+function lightSaveBtn(evt){
+	if (!['INPUT', 'SELECT'].includes(evt.target.nodeName)) return;
+	var chgd = false;
+	var frm = evt.target.closest('form');
+	var chgCount = frm.getAttribute('chgcount');
+	switch (evt.target.type){
+		case 'checkbox':
+			if (evt.target.checked !== oSettings[evt.target.name]){
+				chgCount++;
+				evt.target.labels[0].style.backgroundColor = '#ff0';
+			} else {
+				chgCount--;
+				evt.target.labels[0].style.backgroundColor = '';
+			}
+			break;
+		case 'radio':
+			switch (evt.target.name){
+				case 'radColors':
+					if ((evt.target.value == 'dark') != oSettings.btndark) chgd = true;
+					else chgd = false;
+					break;
+				case 'radFolder':
+					switch (evt.target.value){
+						case 'default':
+							if (oSettings.usefolder == true) chgd = true;
+							else chgd = false;
+							break;
+						case 'ext':
+							if (oSettings.usefolder == false || oSettings.customfolder != null) chgd = true;
+							else chgd = false;
+							break;
+						case 'custom':
+							if (oSettings.usefolder == false || oSettings.customfolder == null) chgd = true;
+							else chgd = false;
+							break;
+					}
+					break;
+				case 'radPrivate':
+					if (evt.target.value != oSettings.keepprivate.toString()) chgd = true;
+					else chgd = false;	
+					break;
+				case 'radSaveAs':
+					if (((evt.target.value == 'omit') && (oSettings.saveas != null)) ||
+						((evt.target.value != 'omit') && (evt.target.value != oSettings.saveas))) chgd = true;
+					else chgd = false;	
+					break;
+			}
+			if (chgd){
+				chgCount++;
+				var rads = frm.querySelectorAll('input[name="' + evt.target.name + '"]');
+				for (var i=0; i<rads.length; i++){
+					if (rads[i].getAttribute('value') == evt.target.getAttribute('value')) rads[i].labels[0].style.backgroundColor = '#ff0';
+					else rads[i].labels[0].style.backgroundColor = '';
+				}
+			} else {
+				chgCount--;
+				var rads = frm.querySelectorAll('input[name="' + evt.target.name + '"]');
+				for (var i=0; i<rads.length; i++){
+					rads[i].labels[0].style.backgroundColor = '';
+				}
+			}
+			break;
+		case 'select-one':
+			if (evt.target.value !== oSettings[evt.target.name]){
+				chgCount++;
+				evt.target.labels[0].style.backgroundColor = '#ff0';
+			} else {
+				chgCount--;
+				evt.target.labels[0].style.backgroundColor = '';
+			}
+			break;
+		default:
+			// none of these 
+	}
+	frm.setAttribute('chgcount', chgCount);
+	var btns = frm.getElementsByClassName('savebtn');
+	for (i=0; i<btns.length; i++){
+		if (chgCount > 0) btns[i].style.backgroundColor = '#ff0';
+		else btns[i].style.backgroundColor = '';
+	}
+}
+
+// Attach event handler to the Save buttons
+document.forms[0].addEventListener('click', updatePref, false);
+document.forms[0].addEventListener('change', lightSaveBtn, false);
